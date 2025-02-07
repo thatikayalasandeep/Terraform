a89ffbcfe6cf334dc3b96b498829a7e65f3be9ba
thatikayalasandeep <51982998+thatikayalasandeep@users.noreply.github.com>
2025-02-07 18:41:06 +0000

    intial commit

---
+++ count/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "expense" {
+  count                  = 3
+  ami                    = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t2.micro"
+
+  tags = {
+    Name    = var.instances[count.index]
+    Purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    to_port     = 22
+    from_port   = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    to_port     = 0
+    from_port   = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
