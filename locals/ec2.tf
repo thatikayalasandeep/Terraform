b05d86449cebe4c10bb5deb6c2cab57e1b33b28f
thatikayalasandeep <51982998+thatikayalasandeep@users.noreply.github.com>
2025-02-21 07:12:54 +0000

    intial commit

---
+++ locals/ec2.tf
@@ -0,0 +1,41 @@
+resource "aws_instance" "example" {
+  ami                    = local.ami_id        #"ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  #instance_type          = var.instance_type
+  instance_type = local.instance_type
+  tags = {
+
+    # expense-dev-backend
+    Name    = local.name
+    Purpose = "terraform-practice"
+  }
+
+}
+
+resource "aws_security_group" "allow_tls" {
+
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+}
